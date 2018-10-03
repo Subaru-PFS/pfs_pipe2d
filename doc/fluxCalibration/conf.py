e817762c3ed1d61e641b57af6ceaa25b661f8625
Paul Price <price@astro.princeton.edu>
2018-10-03 14:02:53 -0400

    fluxCal: disable extra blank pages
    So annoying!

--- doc/fluxCalibration/conf.py
+++ doc/fluxCalibration/conf.py
@@ -123,6 +123,9 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
+
+    # Disable extra pages: https://stackoverflow.com/a/5741112/834250
+    'extraclassoptions': 'openany,oneside'
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
