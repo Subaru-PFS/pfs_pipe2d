d8fd78b59f5bc02b54cd0f2fbf00350e383683e4
Paul Price <price@astro.princeton.edu>
2019-05-07 16:48:46 -0400

    build_pfs: allow setting version name
    Allows us to build all packages and give them a particular common
    version name. This will hopefully allow us to avoid clobbering existing
    versions.

--- bin/build_pfs.sh
+++ bin/build_pfs.sh
@@ -6,11 +6,12 @@ usage() {
     echo "" 1>&2
     echo "Requires that the LSST pipeline has already been installed and setup." 1>&2
     echo "" 1>&2
-    echo "Usage: $0 [-b <BRANCH>] [-l] [-t TAG]" 1>&2
+    echo "Usage: $0 [-b <BRANCH>] [-l] [-t TAG] [-v VERSION]" 1>&2
     echo "" 1>&2
     echo "    -b <BRANCH> : name of branch on PFS to install" 1>&2
     echo "    -l : limited install (w/o drp_stella, pfs_pipe2d)" 1>&2
     echo "    -t : tag name to apply" 1>&2
+    echo "    -v : version name to apply" 1>&2
     echo "" 1>&2
     exit 1
 }
@@ -18,23 +19,27 @@ usage() {
 git_version () {
     [ -e ".git" ] || ( echo "Not a git repo" && exit 1 )
     [ -z $(git status --porcelain --untracked-files=no) ] || ( echo "Git repo not clean" && exit 1 )
-    git describe --tags --always
+    git describe --tags --always | sed 's|/|_|g'
 }
 
 build_package () {
     # Build a package from git, optionally checking out a particular version
     local repo=$1  # Repository on GitHub (without the leading "git://github.com/")
     local commit=$2  # Commit/branch to checkout
-    local tag=$3  # Tag to apply
+    local version=$3  # Version name to apply
+    local tag=$4  # Tag to apply
     local repoName=$(basename $repo)
 
     local repoDir=$(basename $repo)
     ( git clone --branch=$commit --single-branch https://github.com/$repo $repoDir || git clone --branch=master --single-branch https://github.com/$repo $repoDir )
     pushd ${repoDir}
 
+    if [ -z "$version" ]; then
+        version=$(git_version)
+    fi
+    eups list $repoName $version && ( echo "Found existing build of ${repoName} ${version}; refusing to clobber, exiting build" && exit 1 )
+
     setup -k -r .
-    scons
-    version=$(git describe --tags --always | sed 's|/|_|g')
     scons_args=" version=$version"
     [ -n "$tag" ] && scons_args+=" --tag=$tag"
     scons install declare ${scons_args}
@@ -48,7 +53,8 @@ build_package () {
 BRANCH="master"
 LIMITED=false
 TAG=
-while getopts ":b:hlt:" opt; do
+VERSION=
+while getopts ":b:hlt:v:" opt; do
     case "${opt}" in
         b)
             BRANCH=${OPTARG}
@@ -58,6 +64,9 @@ while getopts ":b:hlt:" opt; do
             ;;
         t)
             TAG=${OPTARG}
+            ;;
+        v)
+            VERSION=${OPTARG}
             ;;
         h | *)
             usage
@@ -76,10 +85,10 @@ eval $("$EUPS_DIR/bin/eups_setup" "DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}" eups 
 
 setup sconsUtils
 
-build_package Subaru-PFS/datamodel $BRANCH $TAG
-build_package Subaru-PFS/obs_pfs $BRANCH $TAG
+build_package Subaru-PFS/datamodel $BRANCH "$VERSION" "$TAG"
+build_package Subaru-PFS/obs_pfs $BRANCH "$VERSION" "$TAG"
 
 if [ "$LIMITED" = false ]; then
-    build_package Subaru-PFS/drp_stella $BRANCH $TAG
-    build_package Subaru-PFS/pfs_pipe2d $BRANCH $TAG
+    build_package Subaru-PFS/drp_stella $BRANCH "$VERSION" "$TAG"
+    build_package Subaru-PFS/pfs_pipe2d $BRANCH "$VERSION" "$TAG"
 fi
