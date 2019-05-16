588c784aa3234bb983c4c0dd99870131efba17c5
Paul Price <price@astro.princeton.edu>
2019-05-15 23:21:58 -0400

    fixup! build_pfs: get version names from git

--- bin/build_pfs.sh
+++ bin/build_pfs.sh
@@ -33,9 +33,9 @@ build_package () {
     pushd ${repoDir}
 
     version=$(git_version)
-    if $(eups list $repoName $version); then
+    if eups list $repoName $version; then
         local build=1
-        while $(eups list $repoName ${version}+$build); do build=$((build+1)); done
+        while eups list $repoName ${version}+$build; do build=$((build+1)); done
         version="${version}+$build"
     fi
     echo Building version ${version}...
