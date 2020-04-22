e2f9cedafddc18a99a95b0bee471d1d94232e9ff
Paul Price <price@astro.princeton.edu>
2020-04-22 12:32:40 -0400

    add script to run weekly under Jenkins

---
+++ jenkins/jenkins_weekly.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+WORKDIR=/scratch/pprice/jenkins/weekly/$(date --iso-8601)
+RAWDATA=/projects/HSC/PFS/weekly
+CORES=10
+HERE=$(unset CDPATH && cd "$(dirname "$0")" && pwd)
+
+# Ensure the environment is clean
+[ -n "$(which eups)" ] && unsetup eups
+unset CONDA_DEFAULT_ENV CONDA_EXE CONDA_PREFIX CONDA_PROMPT_MODIFIER CONDA_PYTHON_EXE CONDA_SHLVL
+
+# Need these on tiger to get the right environment
+. /etc/profile  # Get "module"
+module load rh/devtoolset/6  # Get modern compiler
+module load git  # For git-lfs
+
+set -ev
+
+# Build the pipeline
+$HERE/bin/install_pfs.sh -t current $WORKDIR/build
+. $WORKDIR/build/loadLSST.bash
+setup pfs_pipe2d
+
+# Run the weekly production test
+$HERE/weekly/process_weekly.sh -d $RAWDATA -r weekly -c $CORES $WORKDIR/process
+$HERE/test_weekly.py --raw=$RAWDATA --rerun=$WORKDIR/process/rerun/weekly
