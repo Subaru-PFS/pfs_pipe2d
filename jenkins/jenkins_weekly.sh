007b78c3cc4fc38a18299062dff3f2f46a2cd982
Paul Price <price@astro.princeton.edu>
2020-05-11 13:27:44 -0400

    jenkins: fix paths for weekly

--- jenkins/jenkins_weekly.sh
+++ jenkins/jenkins_weekly.sh
@@ -3,10 +3,10 @@
 WORKDIR=/scratch/pprice/jenkins/weekly/$(date --iso-8601)
 RAWDATA=/projects/HSC/PFS/weekly
 CORES=10
-HERE=$(unset CDPATH && cd "$(dirname "$0")" && pwd)
+HERE=$(unset CDPATH && cd "$(dirname "$0")/.." && pwd)/
 
 # Ensure the environment is clean
-( [ -n "$(which eups)" ] && unsetup eups ) || echo "No eups in environment."
+( type eups && unsetup eups ) || echo "No eups in environment."
 unset CONDA_DEFAULT_ENV CONDA_EXE CONDA_PREFIX CONDA_PROMPT_MODIFIER CONDA_PYTHON_EXE CONDA_SHLVL
 
 # Need these on tiger to get the right environment
@@ -23,4 +23,4 @@ setup pfs_pipe2d
 
 # Run the weekly production test
 $HERE/weekly/process_weekly.sh -d $RAWDATA -r weekly -c $CORES $WORKDIR/process
-$HERE/test_weekly.py --raw=$RAWDATA --rerun=$WORKDIR/process/rerun/weekly
+$HERE/weekly/test_weekly.py --raw=$RAWDATA --rerun=$WORKDIR/process/rerun/weekly
